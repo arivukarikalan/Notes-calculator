bdafdc3630f466abd076e53e16d31de347fc0ae8
arivazhagan <arivukarikalan7@gmail.com>
2023-08-18 09:33:29 +0530

    added feature

--- app.js
+++ app.js
@@ -4,20 +4,54 @@ document.addEventListener("DOMContentLoaded", function () {
     const calculateWeeklyIncomeButton = document.getElementById("calculateWeeklyIncomeButton");
     const weeklyIncomeDisplay = document.getElementById("totalincome"); // Add an element to display weekly income
     // Load tasks from local storage and display them
+
+    const taskDetails = []; // To store the added task details
+
+    const addTaskButton = document.getElementById("addTaskButton");
+
+    addTaskButton.addEventListener("click", function () {
+    addTaskDetails();
+    });
+
+function addTaskDetails() {
+    const day = document.getElementById("day").value;
+    const taskName = document.getElementById("taskName").value;
+    const wage = parseFloat(document.getElementById("wage").value);
+    const quantity = parseFloat(document.getElementById("quantity").value);
+    const income = wage * quantity;
+
+    taskDetails.push({
+        day:day,
+        taskName: taskName,
+        wage: wage,
+        quantity: quantity,
+        income: income
+    });
+
+    // Clear the input fields
+    document.getElementById("taskName").value = "";
+    document.getElementById("wage").value = "";
+    document.getElementById("quantity").value = "";
+
+    console.log(taskDetails)
+}
+
     function loadTasks() {
         const savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+       
 
         savedTasks.forEach(task => {
+            const taskCard = createTaskCard(task);
+            tasksContainer.appendChild(taskCard);
+        });
+        taskDetails.forEach(task => {
             const taskCard = createTaskCard(task);
             tasksContainer.appendChild(taskCard);
         });
     }
 
     // Save tasks to local storage
-    function saveTasks(tasks) {
-        localStorage.setItem("tasks", JSON.stringify(tasks));
-    }
-
+    
     // Create a task card
     function createTaskCard(task, index) {
         const taskCard = document.createElement("div");
@@ -34,7 +68,8 @@ document.addEventListener("DOMContentLoaded", function () {
                 </div>
             </div>
         `;
-    
+       
+
         taskCard.querySelector("button").addEventListener("click", function () {
             removeTask(index);
             console.log(index)
@@ -42,21 +77,10 @@ document.addEventListener("DOMContentLoaded", function () {
     
         return taskCard;
     }
+  
     
-    function removeTask(index) {
-        const savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
-        savedTasks.splice(index, 1);
-        saveTasks(savedTasks);
-        updateTasksDisplay(savedTasks);
-        totalWeeklyIncome();
-    }
-    function updateTasksDisplay(tasks) {
-        tasksContainer.innerHTML = "";
-        tasks.forEach((task, index) => {
-            const taskCard = createTaskCard(task, index);
-            tasksContainer.appendChild(taskCard);
-        });
-    }
+ 
+   
 
     document.getElementById("clearAllButton").addEventListener("click", clearAllTasks);
     
@@ -83,7 +107,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
         const day = document.getElementById("day").value;
        
-        const taskName = document.getElementById("taskName").value;
+        const taskNameSelect = document.getElementById("taskName"); // Get the dropdown element
+        const taskName = taskNameSelect.options[taskNameSelect.selectedIndex].text; // Get the selected option text
         const wage = parseFloat(document.getElementById("wage").value);
         const quantity = parseFloat(document.getElementById("quantity").value);
       
@@ -97,16 +122,38 @@ document.addEventListener("DOMContentLoaded", function () {
             income: wage * quantity
         };
 
+        taskDetails.push(newTask);
+
         const savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
-        savedTasks.push(newTask);
-        saveTasks(savedTasks);
+        const allTasks = [...savedTasks, ...taskDetails];
 
-        // Create and display task card
-        const taskCard = createTaskCard(newTask);
-        tasksContainer.appendChild(taskCard);
+        // Save combined task data to local storage
+        localStorage.setItem("tasks", JSON.stringify(allTasks));
 
+    // Clear task details array after saving
+         taskDetails.length = 0;
+        loadTasks(); // Load and display all tasks, including added task details
+     
         // Clear form inputs
         form.reset();
     });
-   
+      function removeTask(index) {
+    const savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    console.log(savedTasks)
+    savedTasks.splice(index, 1);
+    saveTasks(savedTasks);
+    updateTasksDisplay(savedTasks);
+    totalWeeklyIncome(savedTasks); 
+    }
+     function updateTasksDisplay(tasks) {
+        tasksContainer.innerHTML = "";
+        tasks.forEach((task, index) => {
+            const taskCard = createTaskCard(task, index);
+            tasksContainer.appendChild(taskCard);
+        });
+    }
+    function saveTasks(tasks) {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
 });
